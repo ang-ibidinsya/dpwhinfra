a59ba9f7af7d0676a0acbb56de8e6756b634db15
Bobo Dev2 <bearhayday@gmail.com>
2024-12-20 01:40:40 +0800

    Loading Indicator and unzipping of data

--- Analyzer/dpwh-analyzer/src/App.jsx
+++ Analyzer/dpwh-analyzer/src/App.jsx
@@ -1,9 +1,52 @@
 import './App.css';
+import { LoadingIndicator } from './components/loadingIndicator';
+import { useEffect, useState } from 'react';
+import JSZip from 'jszip';
 
 function App() {
+  console.log('App Render...');
+  const [finishedLoading, setFinishedLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('useEffect...');
+    const fetchAndLoadData = async () => {
+      console.log('fetchAndLoadData start');
+      const fetchResponse = await fetch('./AllContracts.json.zip');
+      if (!fetchResponse.ok) {
+        console.error('Unable to fetch contracts data!');
+        return;
+      }
+      const contractsZip = await fetchResponse.arrayBuffer();
+      const zip = new JSZip();
+      try{
+        const unzippedFiles = await zip.loadAsync(contractsZip);
+        const contractsFile = unzippedFiles.files['AllContracts.json'];
+        const constractsJson = JSON.parse(await contractsFile.async('string'));
+        console.log('Contracts Length', constractsJson.length);
+      }
+      catch(ex) {
+        console.error('Exception trying to unzip data', ex);
+        return;
+      }
+          
+      setFinishedLoading(true);
+      console.log('fetchAndLoadData finished');
+    }
+
+    fetchAndLoadData();
+  }, []) // Run once only when the component mounts
+
+  // 
+  // const contractsZip = await fetch('AllContracts.json.zip');
+  // console.log('Finished fetching zip file..');
+  // 
+  // console.log('App Prep Finished...');
+  if (!finishedLoading) {
+    return <LoadingIndicator/>;
+  }
   return (
     <div>
-      Hello      
+      Finished Loading: {finishedLoading ? 'Yes' : 'No'}
     </div>
   )
 }
---
+++ Analyzer/dpwh-analyzer/src/components/loadingIndicator.jsx
@@ -0,0 +1,12 @@
+import './loadingIndicator.css';
+
+export const LoadingIndicator = () => {
+    return <div className='loaderPage'>
+        <div className="loaderContainer">            
+            <div className="loader"/>
+            <div className="loaderLabel">Loading...</div>
+        </div>
+    </div>
+    //return <div className="loader"/>;
+
+}
